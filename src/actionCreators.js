28e7731e3447c5bc60638fe1322c4ee82ec39006
Fredrik Olovsson <fogithub@gmail.com>
2017-06-24 20:11:57 +0200

    Add actionTypes and actionCreators

---
+++ src/actionCreators.js
@@ -0,0 +1,23 @@
+import {
+  LOCAL_STORAGE_REMOVE_ITEM,
+  LOCAL_STORAGE_SET_ITEM,
+} from './actionTypes'
+
+export const removeItem = (key) => {
+  return {
+    type: LOCAL_STORAGE_REMOVE_ITEM,
+    payload: {
+      key,
+    }
+  }
+}
+
+export const setItem = (key, value) => {
+  return {
+    type: LOCAL_STORAGE_SET_ITEM,
+    payload: {
+      key,
+      value,
+    }
+  }
+}
---
+++ src/actionTypes.js
@@ -0,0 +1,2 @@
+export const LOCAL_STORAGE_REMOVE_ITEM = '@@localStorage/REMOVE_ITEM'
+export const LOCAL_STORAGE_SET_ITEM = '@@localStorage/SET_ITEM'
