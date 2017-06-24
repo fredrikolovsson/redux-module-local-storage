954df610468b17fcc34ad0601a190d6be7a37c6d
Fredrik Olovsson <fogithub@gmail.com>
2017-06-24 20:14:52 +0200

    Add exports to entry file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as actionCreators from './actionCreators'
+import * as actionTypes from './actionTypes'
+import * as utils from './utils'
+import middleware from './middleware'
+
+export {
+  actionCreators,
+  actionTypes,
+  middleware,
+  utils,
+}
