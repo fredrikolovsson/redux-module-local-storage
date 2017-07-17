f408691fec87c0823a9e52aadbb3e410f4cfed5e
Fredrik Olovsson <fogithub@gmail.com>
2017-07-18 01:07:00 +0200

    Add support for storing objects
    Using JSON.stringify and parse. If the value cannot be parsed it
    will be returned unparsed (e.g. if it is an empty string).

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,14 @@ export const getItem = (key, localStorageImplementation = localStorage) => {
     return console.error('localStorage implementation is missing') // eslint-disable-line no-console
   }
 
-  return localStorageImplementation.getItem(key)
+  const unparsedValue = localStorageImplementation.getItem(key)
+
+  try {
+    const parsedValue = JSON.parse(unparsedValue)
+    return parsedValue
+  } catch (error) {
+    return unparsedValue
+  }
 }
 
 export const removeItem = (key, localStorageImplementation = localStorage) => {
@@ -20,5 +27,5 @@ export const setItem = (key, value, localStorageImplementation = localStorage) =
     return console.error('localStorage implementation is missing') // eslint-disable-line no-console
   }
 
-  return localStorageImplementation.setItem(key, value)
+  return localStorageImplementation.setItem(key, JSON.stringify(value))
 }
