5b159857844acc867e499bd3e90743eea7d3069a
Fredrik Olovsson <fogithub@gmail.com>
2017-07-17 19:00:06 +0200

    Fix untracked files

--- build/actionTypes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var LOCAL_STORAGE_REMOVE_ITEM = exports.LOCAL_STORAGE_REMOVE_ITEM = '@@localStorage/REMOVE_ITEM';
-var LOCAL_STORAGE_SET_ITEM = exports.LOCAL_STORAGE_SET_ITEM = '@@localStorage/SET_ITEM';
